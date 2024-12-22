19d0242c3f18521296e620d1b31b4b219df04b7f
Bunyawat Singkiporn <pupahero@gmail.com>
2024-12-22 17:36:38 +0700

    style: adjust style for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,32 +35,61 @@ const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600">
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
-          <div className="md:py-5 md:block">
-            <h2 className="text-2xl font-bold">Bunyawat Singkiporn</h2>
+          <div className="flex items-center justify-between py-3">
+            <div className="md:py-5 md:block">
+              <h2 className="text-2xl font-bold">Bunyawat Singkiporn</h2>
+            </div>
+            <div className="md:hidden">
+              <button onClick={() => setNavbar(!navbar)}>
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
           </div>
         </div>
-        <div className="md:flex md:space-x-6 md:items-center">
-          {NAV_ITEMS.map((item, index) => {
-            return <a key={index}>{item.label}</a>;
-          })}
-          {currentTheme === "dark" ? (
-            <button
-              onClick={() => setTheme("light")}
-              className="bg-slate-100 p-2 rounded-xl"
-            >
-              <RiSunLine size={25} color="black" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setTheme("dark")}
-              className="bg-slate-100 p-2 rounded-xl"
-            >
-              <RiMoonFill size={25} />
-            </button>
-          )}
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:p-0  md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="md:flex md:space-x-6 md:space-y-0 md:items-center space-y-8">
+              {NAV_ITEMS.map((item, index) => {
+                return (
+                  <Link
+                    key={index}
+                    to={item.page}
+                    onClick={() => setNavbar(!navbar)}
+                    className="block lg:inline-block text-neutral-800 hover:text-neutral-500 dark:text-neutral-100"
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                  >
+                    {item.label}
+                  </Link>
+                );
+              })}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiSunLine size={25} color="black" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className="bg-slate-100 p-2 rounded-xl"
+                >
+                  <RiMoonFill size={25} />
+                </button>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </header>

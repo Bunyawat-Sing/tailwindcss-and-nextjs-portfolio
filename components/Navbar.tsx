5fba96d1a5d21bfe593f3fc16c45283872b66d70
Bunyawat Singkiporn <pupahero@gmail.com>
2024-12-22 16:55:19 +0700

    feat: create navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-scroll";
+import { useTheme } from "next-themes";
+import { RiMoonFill } from "react-icons/ri";
+import { RiSunLine } from "react-icons/ri";
+import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+
+interface NavItem {
+  label: string;
+  page: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: "Home",
+    page: "home",
+  },
+  {
+    label: "About",
+    page: "about",
+  },
+  {
+    label: "Projects",
+    page: "projects",
+  },
+];
+
+const Navbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+    <header className="w-full mx-auto px-4 bg-white shadow fixed top-0 z-50 dark:bg-stone-900 dark:border-b dark:border-stone-600">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div className="md:py-5 md:block">
+            <h2 className="text-2xl font-bold">Bunyawat Singkiporn</h2>
+          </div>
+        </div>
+        <div className="md:flex md:space-x-6 md:items-center">
+          {NAV_ITEMS.map((item, index) => {
+            return <a key={index}>{item.label}</a>;
+          })}
+          {currentTheme === "dark" ? (
+            <button
+              onClick={() => setTheme("light")}
+              className="bg-slate-100 p-2 rounded-xl"
+            >
+              <RiSunLine size={25} color="black" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setTheme("dark")}
+              className="bg-slate-100 p-2 rounded-xl"
+            >
+              <RiMoonFill size={25} />
+            </button>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
